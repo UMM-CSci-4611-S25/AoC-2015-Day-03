598ad325daa4bf6589c261d95c6c43b424fbbf40
Nic McPhee <mcphee@morris.umn.edu>
2025-02-11 16:48:17 -0600

    Convert `.current_pos` into a method
    I had left off the open-close at the end, so what I intended to be function calls in the tests were instead references to fields. This fixes that.

--- src/bin/part1.rs
+++ src/bin/part1.rs
@@ -29,7 +29,7 @@ mod tests {
     //     let mut visited_houses = VisitedHouses::new();
     //     visited_houses.perform_move(Direction::East);
     //     assert_eq!(visited_houses.num_visited_houses(), 2);
-    //     assert_eq!(visited_houses.current_pos, Pos(1, 0));
+    //     assert_eq!(visited_houses.current_pos(), Pos(1, 0));
     // }
 
     // #[test]
@@ -38,7 +38,7 @@ mod tests {
     //     let moves = Moves::from_str("^>v<").unwrap();
     //     visited_houses.perform_moves(moves);
     //     assert_eq!(visited_houses.num_visited_houses(), 4);
-    //     assert_eq!(visited_houses.current_pos, Pos(0, 0));
+    //     assert_eq!(visited_houses.current_pos(), Pos(0, 0));
     // }
 
     // #[test]
@@ -47,7 +47,7 @@ mod tests {
     //     let moves = Moves::from_str("^v^v^v^v^v").unwrap();
     //     visited_houses.perform_moves(moves);
     //     assert_eq!(visited_houses.num_visited_houses(), 2);
-    //     assert_eq!(visited_houses.current_pos, Pos(0, 0));
+    //     assert_eq!(visited_houses.current_pos(), Pos(0, 0));
     // }
 
     // #[test]
